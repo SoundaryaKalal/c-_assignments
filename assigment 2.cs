73370bb267ba91b47aad4049f45f5cb015bfe54c
SoundaryaKalal <104731104+SoundaryaKalal@users.noreply.github.com>
2022-08-22 20:29:06 -0700

    Add files via upload

---
+++ assigment 2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment7
+{
+    internal class assignmnet2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a first number: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input a second number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int minNumber;
+            if(m<n)
+            {
+                minNumber = m;
+            }
+            else
+            {
+                minNumber = n;
+            }
+            Console.WriteLine("minimum number is:" +  minNumber);
+            Console.WriteLine();
+        }
+    }
+}
