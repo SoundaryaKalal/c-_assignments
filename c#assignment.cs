01f8e9b59bcf096f3e20b5611259c7d055033124
SoundaryaKalal <104731104+SoundaryaKalal@users.noreply.github.com>
2022-08-23 04:32:28 -0700

    Update c#assignment.cs

--- c#assignment.cs
+++ c#assignment.cs
@@ -99,18 +99,27 @@ class Exercise4
 //5.Write a C# program to create a new string from a given string where the
 //first and last characters will change their positions
 class Exercise5
+    
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
 {
-    public static void Main()
+    internal class newstring
     {
-        var name = Console.ReadLine();
-        var split_name = name.Split();
-
-
-        for (int i = 0; i <= split_name.Length; i++)
+        static void Main(string[] args)
         {
-            for (int j = 0; j <= i; j++)
-            {
-            }
+            Console.WriteLine(first_last("Eldin"));
+            Console.WriteLine(first_last("mariam"));
+            Console.WriteLine(first_last("alex"));
+        }
+        public static string first_last(string ustr)
+        {
+            return ustr.Length > 1
+                ? ustr.Substring(ustr.Length - 1) + ustr.Substring(1, ustr.Length - 2) + ustr.Substring(0, 1) : ustr;
         }
     }
 }
