c8e09140a9253397aecafaf1f72a45a43cf6dcf0
SoundaryaKalal <104731104+SoundaryaKalal@users.noreply.github.com>
2022-08-24 03:12:09 -0700

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public static int mul_num(int num1, int num2, int num3)
+        {
+            return num1* num2 * num3;
+        }
+        public static void MSain()
+        {
+            int num1, num2, num3;
+
+            Console.Write("Input the first number to multiply: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the second number to multiply: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the third number to multiply: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            int result = mul_num(num1, num2, num3);
+            Console.WriteLine($"the output is {result}");
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/twonum.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace ConsoleApp2
+{
+    internal class SmallNum
+    {
+        public static void Num(int num1, int num2, out int result)
+        {
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+        }
+        public static void Main()
+        {
+            int num1, num2;
+            Console.WriteLine("Enter First Number");
+
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second Number");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Num(num1, num2, out int result);
+            Console.WriteLine($"The smaller number is {result}");
+        }
+
+
+
+
+    }
+}
